cb18a91245338634897bedce361ed27e554fbd0b
Rajdeep Singh <radewala.2@gmail.com>
2025-04-16 17:45:45 +0530

    remove the unused code

--- app/logto.ts
+++ app/logto.ts
@@ -1,13 +1,6 @@
-import { LogtoNextConfig } from "@logto/next";
+import type { LogtoNextConfig } from "@logto/next";
+import { UserScope } from "@logto/next";
 
-// export const logtoConfig: LogtoNextConfig = {
-//   appId: '<your-application-id>',
-//   appSecret: '<your-app-secret-copied-from-console>',
-//   endpoint: '<your-logto-endpoint>', // E.g. http://localhost:3001
-//   baseUrl: '<your-nextjs-app-base-url>', // E.g. http://localhost:3000
-//   cookieSecret: 'complex_password_at_least_32_characters_long',
-//   cookieSecure: process.env.NODE_ENV === 'production',
-// };
 export const logtoConfig: LogtoNextConfig = {
   endpoint: "https://23zae0.logto.app/",
   appId: "2tr4qj87crznp5da3wgsj",
@@ -15,4 +8,5 @@ export const logtoConfig: LogtoNextConfig = {
   baseUrl: "http://localhost:3000", // Change to your own base URL
   cookieSecret: "DAfWKsEuVt96pLhe9Pezahtwfnuoaqfy", // Auto-generated 32 digit secret
   cookieSecure: process.env.NODE_ENV === "production",
+  scopes: [UserScope.Email, UserScope.Phone], // Add more scopes if needed
 };
